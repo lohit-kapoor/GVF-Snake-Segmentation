52666660ae72d712668c60b35a51d6182a57f9d5
Lohit_Kapoor <lohit.kapoor.bme09@iitbhu.ac.in>
2021-04-19 12:14:29 +0530

    made changes

--- GVFOptimizeImageForces.m
+++ GVFOptimizeImageForces.m
@@ -13,11 +13,11 @@ u=Fx;  v=Fy;
 % Iteratively perform the Gradient Vector Flow (GVF)
 for i=1:Iterations,
   % Calculate Laplacian
-  Uxx=ImageDerivatives2D(u,Sigma,'xx');
-  Uyy=ImageDerivatives2D(u,Sigma,'yy');
+  Uxx=ImageDerivatives(u,Sigma,'xx');
+  Uyy=ImageDerivatives(u,Sigma,'yy');
   
-  Vxx=ImageDerivatives2D(v,Sigma,'xx');
-  Vyy=ImageDerivatives2D(v,Sigma,'yy');
+  Vxx=ImageDerivatives(v,Sigma,'xx');
+  Vyy=ImageDerivatives(v,Sigma,'yy');
 
   % Update the vector field
   u = u + Mu*(Uxx+Uyy) - sMag.*(u-Fx);
